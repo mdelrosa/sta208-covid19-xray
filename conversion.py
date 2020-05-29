863282ed7498f3e61591a10ebd6faf82373c3517
anthonynguyen2021 <64521273+anthonynguyen2021@users.noreply.github.com>
2020-05-29 01:18:22 -0700

    Add files via upload
    Added horizontal flip, rotation, gaussian noise, horizontal and vertical shearing, and image padding.

--- conversion.py
+++ conversion.py
@@ -10,12 +10,33 @@ Parameter: The resulting size of the image num_rows x num_cols
 
 How to Run: 
     
+WARNING: 1 conversion type at a time
+    
 Specify below 1) num_rows, 2) num_cols, 3) source_path - folder of all the images (no other file types!) 4) destination_path - an empty folder where the modified images are to be stored
 
 Input names:  
     
 num_rows: number of rows of resized image
 num_cols: number of columns of resized image
+
+resize: If set to True, images are resized to num_rows by num_cols. If set False, pass any numerical values of num_rows and num_cols.
+
+horizontal_reflection: Set True for images to be horizonatally flipped
+rotate_image: Set True for rotation to be enabled
+degree: If rotate_image is set to be True, specify how many degrees of rotation for the images.
+
+gauss_noise: If set to True, we add random noise to images. (Careful: This can make the conversion slower)
+mean: If gauss_noise is set to True, random noise drawn from gaussian noise with mu = mean
+std_dev: If gauss_noise is set to True, random noise drawn from gaussian noise with sigma = std_dev
+
+shear_horizontal: If set to True, horizontally shear images.
+horizontal_factor: If shear_horizontal is set to True, horizontal shear images by factor horizontal_factor.
+
+shear_vertical: If set to True, vertically shear images.
+vertical_factor: If shear_vertical is set to True, vertically shear images by factor vertical_factor.
+
+zero_padding: If set to True, pad the images by zeros and resulting image is padding_dim by padding_dim
+padding_dim: If zero_padding is set to True, the padding images are dimension padding_dim by padding_dim
 
 Pick exactly one (1) pair below: source_path in [train_path,test_path] and destination_path in [train_destination_path,test_destination_path]
 
@@ -31,137 +52,87 @@ test_destination_path = path where resized test images are to be saved
 import numpy as np
 import os
 from PIL import Image
+from PIL import ImageOps
 
 # Function that resize all images in source_path to dimension num_rows by num_cols and saves them in destination_path 
-def convert_image(source_path,destination_path,num_rows,num_cols,label_dict,print_every=100,resize_bool=True):
+def convert_image(source_path,destination_path,num_rows,num_cols,resize,horizontal_reflection,rotate_image,degree,gauss_noise,mean,std_dev,shear_horizontal,horizontal_factor,shear_vertical,vertical_factor,zero_padding,padding_dim):
 
     path = [source_path,destination_path]
-    task = "Resize" if resize_bool else "Move"
-    print(f"--- {task} images in {source_path}. Store outputs in {destination_path} ---")
-
+    
     # Get all the file names in the folder
     list_files = os.listdir(path[0])
     # Number of files in source path
-    num_files = len(list_files)
-    
-    # Files that are problematic and need to be manually processed
-    #list_files_exception = ['acute-respiratory-distress-syndrome-ards.jpg','acute-respiratory-distress-syndrome-ards-1.jpg','SARS-10.1148rg.242035193-g04mr34g07b-Fig7b-day12.jpeg','SARS-10.1148rg.242035193-g04mr34g09a-Fig9a-day17.jpeg','SARS-10.1148rg.242035193-g04mr34g09b-Fig9b-day19.jpeg','SARS-10.1148rg.242035193-g04mr34g09c-Fig9c-day27.jpeg']
+    num_files = len(list_files)   
 
     # Counter for files processed
     files_processed = 1    
-    bad_labels = []
     
     # Iterate over all the images, perform averaging over the RGB channels, rescale to N x N, and save image in destination    
     for file in list_files:
-        if files_processed % print_every == 0:
-            print('-> ' + str(files_processed) + ' files processed out of ' + str(num_files) + ' files.')
+        print(str(files_processed) + ' files processed out of ' + str(num_files) + ' files.')
         files_processed += 1
         # Take an image in name folder and open it
         # Then resize the image to N x N
         image = Image.open(path[0] + '\\' + file)
-        image2 = image.resize((num_rows,num_cols)) if resize_bool else image
-
-        # get label from label_dict
+        # Try averaging over colored image
         try:
-            label = label_dict[file][0] # first element is label
-            
-            # Try averaging over colored image
-            try:
-                # Averaging images over RGB channels
-                image2_array = np.array(image2)
-                image_average = np.zeros((np.shape(image2_array)[0],np.shape(image2_array)[1]))
-                for i in range(3):
-                    image_average += image2_array[:,:,i]
-                image_average /= 3
-                    
-                # Preprocessing so every image is in the format 0-255 by np.uint8
-                # Save image in destination folder in format .jpg
-                image_average = image_average.astype(np.uint8)
-                image3 = Image.fromarray(image_average)
-                image3.save(path[1] + '\\' + label + '\\' + file,'JPEG') 
-            # Gray scale images    
-            except IndexError: 
-                image2.save(path[1] + '\\' + label + '\\' + file,'JPEG')
-        except: # label not found -- skip and increment
-            bad_labels.append(file)
-    print('\n-> Your images are resized at ' + str(num_rows) + ' by ' + str(num_cols) + ' and are in the path ' + destination_path)
-    print(f'--- There were {len(bad_labels)} images without labels ---')
-
-# Function that resize all images in source_path to dimension num_rows by num_cols and saves them in destination_path 
-def sort_images(source_path,destination_path,label_dict,print_every=100):
-    path = [source_path,destination_path]
-    print(f"--- Sort images in {source_path} based on labels {destination_path} ---")
-
-    # Get all the file names in the folder
-    list_files = os.listdir(path[0])
-    # Number of files in source path
-    num_files = len(list_files)
+            # Averaging images over RGB channels
+            image_array = np.array(image)
+            image_average = np.zeros((np.shape(image_array)[0],np.shape(image_array)[1]))
+            for i in range(3):
+                image_average += image_array[:,:,i]
+            image_average /= 3
+                
+            # Preprocessing so every image is in the format 0-255 by np.uint8
+            image_average = image_average.astype(np.uint8)
+            image = Image.fromarray(image_average)
+        # Gray scale images    
+        except IndexError: 
+            pass
+        # Reflection
+        if horizontal_reflection == True:
+            image = ImageOps.mirror(image)
+        # Rotation
+        if rotate_image == True:
+            image = image.rotate(degree)
+        # Resizing 
+        if resize == True:
+            image = image.resize((num_rows,num_cols))
+        # Gaussian Noise
+        if gauss_noise == True:
+            img = np.array(image)  
+            # add Gaussian Noise
+            noisy_img = img + np.random.normal(mean,std_dev,img.shape)
+            # remove out of range pixel values
+            noisy_img_clipped = np.clip(noisy_img,0,255)
+            # reformat to get image to render
+            noisy_img_clipped_format = noisy_img_clipped.astype(np.uint8)
+            image = Image.fromarray(noisy_img_clipped_format)
+        # Shearing Images
+        if shear_horizontal == True:
+            image = image.transform(image.size,Image.AFFINE,(1,horizontal_factor,0,0,1,0))
+        if shear_vertical == True:
+            image = image.transform(image.size,Image.AFFINE,(1,0,0,vertical_factor,1,0))
+        if zero_padding == True:
+            if np.shape(image)[0] < padding_dim and np.shape(image)[1] < padding_dim:
+                zero_matrix = np.zeros((padding_dim,padding_dim))
+                image_matrix = np.array(image)
+                zero_matrix[((padding_dim-image_matrix.shape[0])//2):image_matrix.shape[0]+(((padding_dim-image_matrix.shape[0])//2)),(((padding_dim-image_matrix.shape[1])//2)):image_matrix.shape[1]+(((padding_dim-image_matrix.shape[1])//2))] = image_matrix
+                zero_matrix_format = zero_matrix.astype(np.uint8)
+                image = Image.fromarray(zero_matrix_format)
+            else:
+                print('no padding for image' + file)
+        # Save image in destination folder in format .jpg
+        image.save(path[1] + '\\' + file,'JPEG')
+    if resize == True:
+        print('\nYour images are resized at ' + str(num_rows) + ' by ' + str(num_cols) + ' and are in the path ' + destination_path)
     
-    # Counter for files processed
-    files_processed = 1    
-    bad_labels = []
+# Specify paths
     
-    # Iterate over all the images, perform averaging over the RGB channels, rescale to N x N, and save image in destination    
-    for file in list_files:
-        if files_processed % print_every == 0:
-            print('-> ' + str(files_processed) + ' files processed out of ' + str(num_files) + ' files.')
-        files_processed += 1
-        # Take an image in name folder and open it
-        image = Image.open(path[0] + '\\' + file)
+train_path = 'C:\\Users\\Tony Nguyen\\Desktop\\Coronahack_STAT208\\coronahack-chest-xraydataset\\Coronahack-Chest-XRay-Dataset\\Coronahack-Chest-XRay-Dataset\\train'
+train_destination_path = 'C:\\Users\\Tony Nguyen\\Desktop\\Coronahack_STAT208\\coronahack-chest-xraydataset\\Coronahack-Chest-XRay-Dataset\\Coronahack-Chest-XRay-Dataset\\train_modified'
+test_path = 'C:\\Users\\Tony Nguyen\\Desktop\\Coronahack_STAT208\\coronahack-chest-xraydataset\\Coronahack-Chest-XRay-Dataset\\Coronahack-Chest-XRay-Dataset\\test'
+test_destination_path = 'C:\\Users\\Tony Nguyen\\Desktop\\Coronahack_STAT208\\coronahack-chest-xraydataset\\Coronahack-Chest-XRay-Dataset\\Coronahack-Chest-XRay-Dataset\\test_modified'
 
-        # get label from label_dict
-        try:
-            label = label_dict[file][0] # first element is label
-            
-            # Try averaging over colored image
-            try:
-                # Averaging images over RGB channels
-                image_array = np.array(image)
-                image_average = np.zeros((np.shape(image_array)[0],np.shape(image_array)[1]))
-                for i in range(3):
-                    image_average += image_array[:,:,i]
-                image_average /= 3
-                    
-                # Preprocessing so every image is in the format 0-255 by np.uint8
-                # Save image in destination folder in format .jpg
-                image_average = image_average.astype(np.uint8)
-                image2 = Image.fromarray(image_average)
-                image2.save(path[1] + '\\' + label + '\\' + file,'JPEG') 
-            # Gray scale images    
-            except IndexError: 
-                image.save(path[1] + '\\' + label + '\\' + file,'JPEG')
-        except: # label not found -- skip and increment
-            bad_labels.append(file)
-    print('\n-> Your images are sorted in the path ' + destination_path)
-    print(f'--- There were {len(bad_labels)} images without labels ---')
-   
-def get_target_paths(base_path):
-    train_path = f'{base_path}\\train'
-    train_dest = f'{base_path}\\train_modified'
-    test_path = f'{base_path}\\test'
-    test_dest = f'{base_path}\\test_modified'
-    return [train_path, train_dest, test_path, test_dest]
-
-def check_target_paths(target_paths, label_names):
-    for path in target_paths:
-        for label in label_names:
-            try:
-                os.stat(f'{path}\\{label}')
-                print(f"-> Verified target path: {path}\\{label}")
-            except:
-                print(f"-> '{path}\\{label}' does not exist. Create path.")
-                os.mkdir(f'{path}\\{label}')
-
-if (__name__ == "__main__"):
-    # Specify paths
-
-    # train_path = 'C:\\Users\\Tony Nguyen\\Desktop\\Coronahack_STAT208\\coronahack-chest-xraydataset\\Coronahack-Chest-XRay-Dataset\\Coronahack-Chest-XRay-Dataset\\train'
-    # train_destination_path = 'C:\\Users\\Tony Nguyen\\Desktop\\Coronahack_STAT208\\coronahack-chest-xraydataset\\Coronahack-Chest-XRay-Dataset\\Coronahack-Chest-XRay-Dataset\\train_modified'
-    # test_path = 'C:\\Users\\Tony Nguyen\\Desktop\\Coronahack_STAT208\\coronahack-chest-xraydataset\\Coronahack-Chest-XRay-Dataset\\Coronahack-Chest-XRay-Dataset\\test'
-    # test_destination_path = 'C:\\Users\\Tony Nguyen\\Desktop\\Coronahack_STAT208\\coronahack-chest-xraydataset\\Coronahack-Chest-XRay-Dataset\\Coronahack-Chest-XRay-Dataset\\test_modified'
-    base_path = 'data\\Coronahack-Chest-XRay-Dataset\\Coronahack-Chest-XRay-Dataset'
-    train_path, train_dest, test_path, test_dest = get_target_paths(base_path)
-
-    # Run the image conversion
-    check_target_paths([train_dest, test_dest])
-    convert_image(test_path,test_dest,1000,500)
+# Run the image conversion
+convert_image(train_path,train_destination_path,1000,500,False,False,False,10,False,50,1,False,0.2,False,0.2,False,2500)
